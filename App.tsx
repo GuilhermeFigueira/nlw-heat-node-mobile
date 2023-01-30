8f9e5400c66f1452394a87b487bfd6872414ff4d
Guilherme Figueira <guibf2004@gmail.com>
2023-01-30 16:40:44 -0300

    bibliotecas de loading e estilização

--- App.tsx
+++ App.tsx
@@ -1,5 +1,28 @@
 import { Home } from "./src/screens/Home";
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-	return <Home />;
+	const [fontsLoaded] = useFonts({
+		Roboto_400Regular,
+		Roboto_700Bold,
+	});
+
+	if (!fontsLoaded) {
+		SplashScreen.hideAsync();
+	}
+
+	return (
+		<>
+			<StatusBar style="light" />
+			<Home />
+		</>
+	);
 }
